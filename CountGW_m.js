7c652f0678ace9836d27b03cf90834937098971a
yangting <35136158+YTangIT@users.noreply.github.com>
2019-04-09 17:35:05 +0800

    Add files via upload

---
+++ CountGW_m.js
@@ -0,0 +1,20 @@
+// Gridsum tracking code begin.
+(function () {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = (location.protocol == 'https:' ? 'https://ssl.' : 'http://static.') + 'gridsumdissector.com/js/Clients/GWD-002548-45F840/gs.js';
+        var    firstScript = document.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(s, firstScript);
+})();
+	
+//Gridsum tracking code end.
+
+//CNZZ
+var cnzz_str='<div style="display:none">';
+    cnzz_str+='<script src="https://s19.cnzz.com/z_stat.php?id=1252904335&web_id=1252904335" language="JavaScript"></script>';
+	cnzz_str+='</div>';
+	document.write(cnzz_str);
+//<div style="display:none">
+//<script src="https://s19.cnzz.com/z_stat.php?id=1252904335&web_id=1252904335" language="JavaScript"></script>
+//</div>
